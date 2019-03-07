ca7c9f09e648c23654f6bb18d4a48def7d2446c2
Mathias Elken <math079e@edu.eal.dk>
2019-03-07 13:08:38 +0100

    Domain Layer indsat

---
+++ Doman Layer/Compensations/Compensation.cs
@@ -0,0 +1,43 @@
+﻿using Domain_Layer.Appendices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Layer.Compensations
+{
+    public abstract class Compensation : ISavable
+    {
+        protected static readonly string ConnectionString = "Server=EALSQL1.eal.local; Database=B_DB17_2018; User Id=B_STUDENT17; Password=B_OPENDB17;";
+        public int Id { get; internal set; }
+        protected readonly List<Appendix> appendices = new List<Appendix>();
+        public readonly string Title;
+        internal readonly Employee Employee;
+
+        protected Compensation(string title, Employee employee)
+        {
+            Title = title;
+            Employee = employee;
+        }
+
+        public void AddAppendix(Appendix expense)
+        {
+            appendices.Add(expense);
+        }
+
+        public void RemoveAppendix(Appendix expense)
+        {
+            appendices.Remove(expense);
+        }
+
+        public abstract List<Appendix> GetAppendices();
+
+        public int CountAppendices()
+        {
+            return appendices.Count;
+        }
+
+        public abstract void Save();
+    }
+}
---
+++ Doman Layer/Department.cs
@@ -0,0 +1,36 @@
+﻿using Domain_Layer.Compensations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Layer
+{
+    public class Department
+    {
+        private readonly List<Compensation> compensations = new List<Compensation>();
+        public readonly int Id;
+
+        internal Department(int id)
+        {
+            Id = id;
+        }
+
+        public void AddCompensation(Compensation compensation)
+        {
+            compensation.Save();
+            compensations.Add(compensation);
+        }
+
+        public void RemoveCompensation(Compensation compensation)
+        {
+            compensations.Remove(compensation);
+        }
+
+        public IList<Compensation> GetAllCompensations()
+        {
+            return compensations.AsReadOnly();
+        }
+    }
+}
